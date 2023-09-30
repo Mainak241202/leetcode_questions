952e1fe481d0ba623f65a849396c565c9609964a
Mainak01 <mainak9990@gmail.com>
2023-09-30 10:46:31 +0530

    Time: 0 ms (100%), Space: 8 MB (24.82%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int prev = nums[0],prev2 = 0;
+        for(int i=1;i<n;i++){
+            int pick = nums[i];
+            if(i>1) pick+=prev2;
+            int notpick = 0 + prev;
+            int curi = max(pick,notpick);
+            prev2 = prev;
+            prev = curi;
+        }
+        return prev;
+    }
+};
