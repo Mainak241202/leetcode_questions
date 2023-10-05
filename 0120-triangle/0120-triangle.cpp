031030cb514937b4f68aa26b80dd3ad16b0e2daf
Mainak01 <mainak9990@gmail.com>
2023-10-05 18:56:03 +0530

    Time: 2 ms (92.11%), Space: 9.4 MB (12.33%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& tri) {
+        int n = tri.size();
+        int m = tri[0].size();
+
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for(int j=0;j<n;j++){
+            dp[n-1][j] = tri[n-1][j];
+        }
+
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                if(dp[i][j]!=0) return tri[i][j];
+                int down = tri[i][j] + dp[i+1][j];
+                int diag = tri[i][j] + dp[i+1][j+1];
+
+                dp[i][j] = min(down,diag);
+                }
+            }
+        
+        return dp[0][0];
+    }
+};
