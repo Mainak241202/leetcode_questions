7a6d896c907daadd8ae2e34e0b25dbeb79584eda
Mainak01 <mainak9990@gmail.com>
2023-12-12 00:06:34 +0530

    Time: 0 ms (100%), Space: 11.1 MB (38.26%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> v;
+
+    void generate(vector<int> &temp,int i,vector<int> &nums){
+        if(i==nums.size()){
+            v.push_back(temp);
+            return;
+        }
+        generate(temp,i+1,nums);
+        temp.push_back(nums[i]);
+        generate(temp,i+1,nums);
+        temp.pop_back();
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;
+        generate(temp,0,nums);
+        return v;
+    }
+};
