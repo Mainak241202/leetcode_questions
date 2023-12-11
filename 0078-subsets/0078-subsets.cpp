016cdec05742256ef629bbe7a4892e6b759aee47
Mainak01 <mainak9990@gmail.com>
2023-12-12 01:41:26 +0530

    Time: 4 ms (37.25%), Space: 7.5 MB (59.64%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -7,10 +7,11 @@ public:
             v.push_back(temp);
             return;
         }
-        generate(temp,i+1,nums);
+        //generate(temp,i+1,nums);
         temp.push_back(nums[i]);
         generate(temp,i+1,nums);
         temp.pop_back();
+        generate(temp,i+1,nums);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<int> temp;
