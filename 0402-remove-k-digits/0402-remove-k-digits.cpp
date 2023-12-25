5ded6f2076b8603f341cf68944c16c098be92d8c
Mainak01 <mainak9990@gmail.com>
2023-12-26 03:39:30 +0530

    Time: 12 ms (55.37%), Space: 9.2 MB (51.92%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -5,6 +5,7 @@ public:
         int n = num.size();
         stack<char> st;
         st.push(num[0]);
+        //checks if incoming number is smaller than the top element of stack or not
         for(int i=1;i<n;i++){
            while(!st.empty() && st.top()>num[i] && k>0){
                st.pop();
@@ -13,16 +14,19 @@ public:
            st.push(num[i]);
         }
 
+        //handles the cases for examples such as num="12345"
         while(k>0){
             st.pop();
             k--;
         }
 
+        //adds the elements of stack to the string
         while(!st.empty()){
             s+=st.top();
             st.pop();
         }
 
+        //removes the leading zeros from string
         while(!s.empty() && s.back()=='0'){
             s.pop_back();
         }
